18e6676f4305582cd4cce444ff4869184359f238
Tommy <tbkknight1@icloud.com>
2022-01-10 10:52:16 -0600

    added displayArray which loops through the obejects in the array then call addBookCard, with takes in the info of the book and add that to the dom with the corresponding classes.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: "Something", author: "hfase", pagenum: "1231", hasRead: 'Not Read'
+    },
+
+];
 
 
 function Book(title, author, pagenum, hasRead) {
@@ -25,8 +30,62 @@ function addBookToLibrary() {
 
 function displayArray() {
     for (let i = 0; i < myLibrary.length; i++) {
-        
+        const title = myLibrary[i].title;
+        const author = myLibrary[i].author;
+        const pagenum = myLibrary[i].pagenum;
+        const hasRead = myLibrary[i].hasRead;
+        addBookCard(title, author, pagenum, hasRead);
+
     }
+
+
+}
+
+function addBookCard(title, author, pagenum, hasRead) {
+    const book = document.createElement('div');
+    const titleDom = document.createElement('div');
+    const authorDom = document.createElement('div');
+    const pagesDom = document.createElement('div');
+    const readDom = document.createElement('button');
+    const bottomWrapper = document.createElement('div');
+    const editBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+
+    book.classList.add('book');
+    titleDom.classList.add('title');
+    authorDom.classList.add('author');
+    pagesDom.classList.add('pages');
+    readDom.classList.add('read-status');
+    bottomWrapper.classList.add('bottom-wrapper');
+    editBtn.classList.add('edit-btn');
+    deleteBtn.classList.add('delete-btn');
+
+    titleDom.textContent = `Tiltle: ${title}`;
+    authorDom.textContent = `Author: ${author}`;
+    pagesDom.textContent = `Pages; ${pagenum}`;
+    readDom.textContent = `${hasRead}`;
+
+    editBtn.textContent = 'edit';
+    deleteBtn.textContent = 'delete';
+
+    bottomWrapper.appendChild(editBtn);
+    bottomWrapper.appendChild(deleteBtn);
+    book.appendChild(titleDom);
+    book.appendChild(authorDom);
+    book.appendChild(pagesDom);
+    book.appendChild(readDom);
+    book.appendChild(bottomWrapper);
+
+    const mainColunm = document.querySelector('.main-column');
+
+    // console.log(book);
+    // console.log(mainColunm);
+
+    mainColunm.appendChild(book);
+
+
+
+
 
 
 }
